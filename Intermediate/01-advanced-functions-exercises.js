0e8d9fde631d1cae548b0697f894868dcab1d805
adrirosel <roselmartinezadrian@gmail.com>
2026-08-22 00:32:22 +0200

    Resolucion del resto de ejercicios pendientes sobre funciones avanzadas

--- Intermediate/01-advanced-functions-exercises.js
+++ Intermediate/01-advanced-functions-exercises.js
@@ -5,9 +5,9 @@ const saludar = function saludo(name){
 }
 
 function obtenerSaludo(name){
-    return saludar
+    return saludar(name)
 }
-
+obtenerSaludo("Adrian")
 //2. Implementa una funcion currificada que multiplique 3 numeros
 
 //a * b * c
@@ -46,7 +46,7 @@ console.log(potencia(5,5)) //3125
 function createCounter(valor){
     let valorInicial = valor
     return { 
-        Increment(){
+        increment(){
             valorInicial++
             console.log(`Valor: ${valorInicial}`)
         },
@@ -69,19 +69,99 @@ console.log(counter.getValue())
  */
 
 let counter = createCounter(1)
-counter.Increment()
-counter.Increment()
-counter.Increment()
-counter.decrement()
+counter.increment()
+counter.increment()
+counter.increment()
+counter.increment()
 console.log(counter.getValue())
-//5. Crea una funcion sumManyTimes(multiplier, ...numbers) que primero sume todos los numeros (usando parametros Rest) y luego multiplique el resultado por multiplier
+
+
+//5. Crea una funcion sumManyTimes(multiplier, ...numbers) que primero sume 
+// todos los numeros (usando parametros Rest) y luego multiplique el resultado por multiplier
+
+/* function sumManyTimes(multiplier, ...numbers){
+    let suma = 0
+    for(let i = 0; i<numbers.length; i++){
+        suma+= numbers[i]
+    }
+    return suma * multiplier
+} */
+
+const sumManyTimes = (multiplier, ...numbers) =>{
+    let suma = 0
+    for(let i = 0; i<numbers.length; i++){
+        suma+= numbers[i]
+    }
+    return suma * multiplier
+}
+console.log(sumManyTimes(2, 2, 3, 4, 5, 6))
+
+
 
 //6. Crea un callback que se invoque con el resultado de la suma de todos los numeros que se le pasan a una funcion
 
+function sumaTotal(numbers, callback){
+    let result = 0
+    for(let num of numbers){
+        result += num
+    }
+    callback(result)
+}
+
+function processResult(result){
+    console.log(result)
+}
+
+sumaTotal([1, 2, 3, 4, 5], processResult)
+
 //7. Desarrolla una funcion parcial
+
+function sumaTotal(numbers){
+    let sumTotal = 0
+    for(let num of numbers){
+        sumTotal += num
+    }
+    return function(numberOfNumbers){
+        return sumTotal / numberOfNumbers
+    }
+}
+let numeros = [1, 2, 3, 4, 5, 6]
+const sumarNumeros = sumaTotal(numeros)
+console.log(sumarNumeros(numeros.length))
 
 //8. Implementa un ejemplo que haga uso de Spread 
 
+function date(dia, mes, año){
+    return `${dia}/${mes}/${año}`
+}
+
+let fecha = [27, 9, 2026]
+
+console.log(date(...fecha))
 //9. Implementa un retorno implicito
 
+const multiplicacion = (a, b) => a * b
+console.log(multiplicacion(6, 7))
+
 //10. Haz uso del this lexico
+
+let gimnasio = {
+    nombre : "GymFit",
+    horario : "07:00 - 22:30",
+    mancuernas : [10, 20, 30, 40],
+    matriculacion(){
+        setTimeout(()=>{
+            console.log(`Bienvenido a ${this.nombre}`)
+        }, 4000)
+    },
+    informacion(){
+        console.log(`Informacion de ${this.nombre}: 
+            disponemos del siguiente horario: ${this.horario}
+            y de la siguiente variedad de pesos en las mancuernas: ${this.mancuernas}`)
+    } 
+
+}
+gimnasio.matriculacion()
+gimnasio.informacion()
+
+
